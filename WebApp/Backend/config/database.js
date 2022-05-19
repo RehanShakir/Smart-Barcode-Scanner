5b8bc43f207617644431bf1967085eabe004f3b0
RehanShakir <rehanshakir2@gmail.com>
2022-05-19 12:57:44 +0500

    Frontend: Several Fixed, Backend: Fixed Auth Controller

--- WebApp/Backend/config/database.js
+++ WebApp/Backend/config/database.js
@@ -1,4 +1,16 @@
 const mongoose = require("mongoose");
+
+const options =
+  (process.env.NODE_ENV === "development" && {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }) ||
+  (process.env.NODE_ENV === "production" && {
+    user: process.env.MONGO_INITDB_ROOT_USERNAME,
+    pass: process.env.MONGO_INITDB_ROOT_PASSWORD,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
 exports.connect = () => {
   mongoose
     .connect(process.env.DatabaseURL, {
